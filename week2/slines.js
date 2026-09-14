602896369ae45ed5b442c69963fb0163ef7cbbe8
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 21:09:00 -0400

    lines

--- week2/slines.js
+++ week2/slines.js
@@ -6,20 +6,20 @@ function setup() {
 
   translate(windowWidth/2, windowHeight/2)
   circle(0, 0, 10)
-  
 }
 //line(x1, y1, x2, y2)
 //goal - Draw a continous line that randomly chooses next position 
 
-let currentX= 0// windowWidth/2;
-let currentY= 0//windowHeight/2;
+let currentX= 0//Width/2;
+let currentY= 0//Height/2;
 let nextX = 0;
 let nextY = 0;
 
 function draw() {
-
-  nextY = currentY + random(-25, 25);
-  nextX = currentX + random(-25, 25);
+  stroke("#FFFFFF")
+ 
+  nextY = currentY + random(-12, 12);
+  nextX = currentX + random(-12, 12);
 
   line(currentX, currentY, nextX, nextY);
   currentX = nextX;
