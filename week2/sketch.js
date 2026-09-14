299883fe55b2b83a6067ea60bc13e3201e5e19d3
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 20:42:34 -0400

    Update sketch.js

--- week2/sketch.js
+++ week2/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   translate(windowWidth/2, windowHeight/1.25);
   circle(0, 0, 10);
   stroke("#FFFFFF")
-  line(0,0,100,200)
+ 
 }
 
 
@@ -20,15 +20,19 @@ function draw() {
 //goal 
 // when mouse click generate a branch
 //maybe additional clicks creates mutiple branches
+//prob have to use arrays to store branching positions
 
 
 
+let startx= random(-50, 50)
+let starty= random(-50, -100)
 
-// let randx =
-// let randy =
+let branchx = startx + random(-50, 50)
+let branchy = starty - random(-50, -100)
 
 function mousePressed() {
   translate(windowWidth/2, windowHeight/1.25);
+  line(0,0,0,-50)
   
   for (let i = 0; i < 6; i ++) {
     
