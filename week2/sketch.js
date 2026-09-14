c371773ec800e22a0f459dda529142fa69801f22
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 20:07:31 -0400

    branches

---
+++ week2/sketch.js
@@ -0,0 +1,20 @@
+
+//Branching lines
+//line(x1, y1, x2, y2)
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0)
+  translate(windowWidth/2, windowHeight/2)
+  circle(0, 0, 10)
+}
+
+
+
+
+function draw() {
+  
+}
+
+function mousePressed() {
+  // Code to run.
+}
