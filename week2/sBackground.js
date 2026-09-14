e538754e5877151cf1ee20e300188ec8c6363916
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-14 19:35:52 -0400

    camera controls

--- week2/sBackground.js
+++ week2/sBackground.js
@@ -1,10 +1,11 @@
 // https://p5js.org/tutorials/coordinates-and-transformations/
 // https://p5js.org/tutorials/custom-geometry/
 // https://p5js.org/tutorials/lights-camera-materials/#camera-and-view
-// https://p5js.org/reference/p5/camera/
+// https://p5js.org/reference/p5/p5.Camera/
+// https://beta.p5js.org/reference/p5/keyIsDown/
 
 
-
+let cam;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -12,24 +13,35 @@ function setup() {
   debugMode()
 
   describe('A red box on a white background.');
+
+  cam = createCamera();
+
 }
+
 function draw() {
   background(0);
   // "#315329"
   fill(255, 0, 0);
   box();
 
-  for (let i = 0; i < windowWidth; i += 50) {{
-        push()
-        fill(255, 255, 255)
-        translate(i, 0 , 0 )
-        sphere()
-    }
-    pop()
-  }
+    // if (mouseIsPressed) {
+    // cam.move(0, 0, -1);
+  // }
 
-  
+if (keyIsDown(UP_ARROW)) {
+  cam.move(0, 0, -2); // forward
+}
+else if (keyIsDown(DOWN_ARROW)) {
+  cam.move(0, 0, 2); // backward
+}
+else if (keyIsDown(LEFT_ARROW)) {
+  cam.move(-2, 0, 0); // left
+}
+else if (keyIsDown(RIGHT_ARROW)) {
+  cam.move(2, 0, 0); // right
+}
+orbitControl();
 
 
-  orbitControl();
+
 }
