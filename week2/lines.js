f17af3bb2eeb4b9fd52555505e6485000ae94e6a
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 20:59:16 -0400

    lines

--- week2/lines.js
+++ week2/lines.js
@@ -4,15 +4,15 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0)
 
-  let currentX= windowWidth/2;
-  let currentY= windowHeight/2;
-
-  circle(currentX, currentY, 10)
+  translate(windowWidth/2, windowHeight/2)
+  circle(0, 0, 10)
   
 }
 //line(x1, y1, x2, y2)
 //goal - Draw a continous line that randomly chooses next position 
 
+let currentX= 0// windowWidth/2;
+let currentY= 0//windowHeight/2;
 let nextX = 0;
 let nextY = 0;
 
@@ -22,8 +22,6 @@ function draw() {
   nextX = currentX + random(-25, 25);
 
   line(currentX, currentY, nextX, nextY);
-
-
   currentX = nextX;
   currentY = nextY;
   
