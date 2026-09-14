52553f29602feebc728545709508ad71a3b95cee
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 20:53:03 -0400

    line

--- week2/sketch.js
+++ week2/sketch.js
@@ -5,17 +5,26 @@ function setup() {
   background(0)
 
   let currentX= windowWidth/2;
-  let CurrentY= windowHeight/2;
+  let currentY= windowHeight/2;
+
+  circle(currentX, currentY, 10)
   
 }
 //line(x1, y1, x2, y2)
 //goal - Draw a continous line that randomly chooses next position 
 
-
-
-
-
+let nextX = 0;
+let nextY = 0;
 
 function draw() {
+
+  nextY = currentY + random(-25, 25);
+  nextX = currentX + random(-25, 25);
+
+  line(currentX, currentY, nextX, nextY);
+
+
+  currentX = nextX;
+  currentY = nextY;
   
 }
