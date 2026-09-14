a89423ac083e174f68c206eefb8506b403337898
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 20:47:54 -0400

    line

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,41 +1,21 @@
 
-//Branching lines
-//line(x1, y1, x2, y2)
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  translate(windowWidth/2, windowHeight/1.25);
-  circle(0, 0, 10);
-  stroke("#FFFFFF")
- 
+  background(0)
+
+  let currentX= windowWidth/2;
+  let CurrentY= windowHeight/2;
+  
 }
+//line(x1, y1, x2, y2)
+//goal - Draw a continous line that randomly chooses next position 
+
+
 
 
 
 
 function draw() {
   
-}
-
-//goal 
-// when mouse click generate a branch
-//maybe additional clicks creates mutiple branches
-//prob have to use arrays to store branching positions
-
-
-
-let startx= random(-50, 50)
-let starty= random(-50, -100)
-
-let branchx = startx + random(-50, 50)
-let branchy = starty - random(-50, -100)
-
-function mousePressed() {
-  translate(windowWidth/2, windowHeight/1.25);
-  line(0,0,0,-50)
-  
-  for (let i = 0; i < 6; i ++) {
-    
-    line(0, 0, random(-50, 50), random(-50, -100));
-  }
 }
