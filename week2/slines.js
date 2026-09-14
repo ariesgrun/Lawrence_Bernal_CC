2ced93412db92552bb3f5b661ffb3431f4fe8b21
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-13 21:57:50 -0400

    ;

--- week2/slines.js
+++ week2/slines.js
@@ -2,10 +2,10 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0)
+  background(0);
   currentX= windowWidth/2;
   currentY= windowHeight/2;
-  frameRate(24)
+  frameRate(24);
 }
 //line(x1, y1, x2, y2)
 //goal - Draw a continous line that randomly chooses next position 
@@ -26,22 +26,22 @@ function draw() {
   }
 
   if (nextX > windowWidth) {
-    nextX = windowWidth
+    nextX = windowWidth;
   }
   else if (nextX < 0 ) {
-    nextX = 0
+    nextX = 0;
   }
 
   if (nextY > windowHeight) {
-    nextY = windowHeight
+    nextY = windowHeight;
   }
   else if (nextY < 0 ) {
-    nextY = 0
+    nextY = 0;
   }
   let r = map(currentX, 0, windowWidth, 0, 255);
   let g = map(currentY, 0, windowHeight, 0, 255);
   // stroke(random(0,255), random(0,255), random(0,255))
-  stroke(r , g , 100)
+  stroke(r , g , 100);
   line(currentX, currentY, nextX, nextY);
   currentX = nextX;
   currentY = nextY;
