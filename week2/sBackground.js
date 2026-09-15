0704c6c8bd48d5807a0318362850c9b1bb2260f1
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-14 20:34:06 -0400

    random spawning sphere

--- week2/sBackground.js
+++ week2/sBackground.js
@@ -3,19 +3,27 @@
 // https://p5js.org/tutorials/lights-camera-materials/#camera-and-view
 // https://p5js.org/reference/p5/p5.Camera/
 // https://beta.p5js.org/reference/p5/keyIsDown/
+let spherex = 0;
+let spherey =0 ;
+let spherez = 0;
 
+let nextX;
+let nextY;
+let nextZ;
 
 let cam;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   
-  debugMode()
+  // debugMode()
 
   describe('A red box on a white background.');
 
   cam = createCamera();
   angleMode(DEGREES)
+
+  frameRate(24);
 }
 
 function draw() {
@@ -27,16 +35,16 @@ function draw() {
   // }
 
 if (keyIsDown(UP_ARROW)) {
-  cam.move(0, 0, -2); // forward
+  cam.move(0, 0, -10); // forward
 }
 else if (keyIsDown(DOWN_ARROW)) {
-  cam.move(0, 0, 2); // backward
+  cam.move(0, 0, 10); // backward
 }
 else if (keyIsDown(LEFT_ARROW)) {
-  cam.move(-2, 0, 0); // left
+  cam.move(-10, 0, 0); // left
 }
 else if (keyIsDown(RIGHT_ARROW)) {
-  cam.move(2, 0, 0); // right
+  cam.move(10, 0, 0); // right
 }
 orbitControl();
 
@@ -69,18 +77,41 @@ for (let i = 0; i < 6; i++) {
   pop();
   
 }
-push();
-translate(spherex, spherey, spehrez);
-fill(0,0,255)
-sphere(radius, detailX, detailY);
 
+// nextX = random(-12, 12);
+// nextY =random(-12, 12);
+// nextZ =random(-12, 12);
+
+// spherex = spherex + nextX;
+// spherey = spherey + nextY;
+// spherez = spherez + nextZ;
+
+//RANDOM MOVEMENT ^^
+
+
+let nextX = (noise(0.045 * frameCount) - 0.5) * 10;
+let nextY = (noise(0.045 * frameCount + 1000) - 0.5) * 10;
+let nextZ = (noise(0.045 * frameCount + 2000) - 0.5) * 10;
+
+
+spherex = spherex + nextX;
+spherey = spherey + nextY;
+spherez = spherez + nextZ;
+
+
+
+push();
+translate(spherex, spherey, spherez);
+fill(0,0,255);
+sphere(20);
 pop();
+
+push();
+translate(random(-200,200), random(-200,200), random(-200,200));
+fill(0,0,255);
+sphere(20);
+pop();
+
 }
 
-let spherex;
-let spherey;
-let spehrez;
 
-let nextX;
-let nextY;
-let nextZ;
