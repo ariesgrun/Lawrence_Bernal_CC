f753ce53ba7134454f58a79967e399d9a8d2f569
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-19 18:52:27 -0400

    Create OandC.js

---
+++ notes/OandC.js
@@ -0,0 +1,18 @@
+
+
+
+///  object contains data and functionality
+//class - template for building instances of an object
+
+class walker {
+    constructor() {//objects have a constructor, where they are initialized
+        this.x = width/2;
+        this.y = width/2;
+        //this keyword is used to attach the propertiesto the newly created object itself
+        //methods are function defined in class
+        
+
+
+        
+    }
+}
