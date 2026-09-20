3a3bb4fe7470ddf7e14874c2c0cb1fb07f4713a5
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-20 00:03:19 -0400

    pattern

--- week3/sketch.js
+++ week3/sketch.js
@@ -4,12 +4,13 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background("#223843")
   angleMode(DEGREES)
-  frameRate(24)
+  
 }
 let r = 0; 
 
 function draw() {
 translate(width/2, height/2);
+
 for (let i = 1; i < 11; i++) {
   push();
   noFill();
@@ -22,11 +23,21 @@ for (let i = 1; i < 11; i++) {
   //triangle(x1, y1, x2, y2, x3, y3)
   noFill();
   stroke("#EAD637");
-  rotate(r* 10* i)
+
+  rotate(r* 10* i);
+  stroke("#5E0B15");
+  line(0, 0, 50*i, 0);
+  stroke("#EAD637");
   translate(50*i, 0);
+  rotate(r * 5)
   triangle(-8, -10, 8, -10, 0, 10);
   pop();
   }
   r++
+  if (r>360) {
+    //https://p5js.org/reference/p5/noLoop/
+    noLoop();
+    
+  }
 
 }
