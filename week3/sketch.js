4b733e746fc23e3c99875c1b42cd59d0724b8cb7
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-19 20:12:48 -0400

    Update sketch.js

--- week3/sketch.js
+++ week3/sketch.js
@@ -3,21 +3,20 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0)
-  rectMode(CENTER)
 }
 
 //https://p5js.org/reference/p5/ceil/
-let cols = ceil(width / rectSize);
-let rows = ceil(height / rectSize);
+let cols;
+let rows;
 
-
+let rectSize = random(10,50);
 
 function draw() {
+  rows = ceil(windowHeight / rectSize);
+  cols = ceil(windowWidth / rectSize);
+  rectSize = random(10,50);
 
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      fill("#FFFFFF");
-      rect(i * 50, j * 50, 50, 50);
-    }
-  }
+  for (let i = 0; i < rows; i++) {
+    rect(i*rows,20,20,20);
+    
 }
