f8fbc24326284d1bc4dfecfe4fd0f19b45dd2776
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-20 01:41:39 -0400

    complete pattern

--- week3/stiles.js
+++ week3/stiles.js
@@ -5,17 +5,46 @@ function setup() {
   background(0)
   rectMode(CENTER)
   angleMode(DEGREES)
+  noLoop();
 }
+///https://p5js.org/reference/p5/arc/
+
 
 function draw() {
-  translate(windowWidth/2, windowHeight/2);
-  fill("#ffffff")
-  
-  //https://p5js.org/reference/p5/arc/
-  rect(0,0,100,100)
+translate(50,50);
 
-  fill(255,0,0);
-  arc(-50, -50, 100, 100,0,90)
-  arc(50,50, 100, 100,180,270)
+  for(let i = 0; i<100 ; i++) {
+    for (let j = 0; j < 100; j++) {  
+      push();
+      fill("#ffffff");
+      noStroke();
+      translate(100*i,100*j);
+      rotate(floor(random(0, 100))*90);
+      rect(0,0,100,100);
+      noFill();
+      stroke(255,0,0)
+      arc(-50, -50, 100, 100,0,90);
+      arc(50,50, 100, 100,180,270);
+      pop();
+      }
+      
+    }
 
 }
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+//  push();
+//   fill("#ffffff");
+//   noStroke();
+//   translate(100*i,0);
+//   rotate(floor(random(0, 100))*90);
+//   rect(0,0,100,100);
+//   fill(255,0,0);
+//   arc(-50, -50, 100, 100,0,90);
+//   arc(50,50, 100, 100,180,270);
+//   pop();
+//   }
