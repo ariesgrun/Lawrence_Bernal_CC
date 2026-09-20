9e1333c70f66c2ab65a25d1cc9fa0f211db610ab
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-20 01:13:14 -0400

    tile pattern

--- week3/stiles.js
+++ week3/stiles.js
@@ -2,9 +2,18 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  background(0)
+  rectMode(CENTER)
 }
 
 function draw() {
+  translate(windowWidth/2, windowHeight/2);
+  fill("#ffffff")
   
+  //https://p5js.org/reference/p5/arc/
+  rect(0,0,100,100)
+
+  fill(255,0,0);
+  arc(0, 0, 100, 100,0,HALF_PI)
+
 }
