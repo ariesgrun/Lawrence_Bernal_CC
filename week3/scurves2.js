94bcd0367445bed460896767109629e9e8846491
Lawrence Bernal <lmbernal412@gmail.com>
2026-09-22 23:13:46 -0400

    pattern

--- week3/scurves2.js
+++ week3/scurves2.js
@@ -1,16 +1,23 @@
 
 
 function setup() {
+
   createCanvas(2560, 1440);
   background("#223843");
-  
 }
 
 function draw() {
-    translate(width/2, height/2);
-  
-    stroke("#4C9A6A");
+  translate(width/2, height/2);
 
+  stroke("#4C9A6A");
 
+  noStroke();
+  circle(0, 0, 10);
+
+  stroke("#4C9A6A");
+
+  //translate(250,250)
+  noFill();
+  bezier(0, 0, 300, -100, 920, 800, 550, 550)
 
 }
